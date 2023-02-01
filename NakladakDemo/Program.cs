5dc155a9a2f60aae218407bba6f032e3bee43452
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-02-01 07:40:57 +0100

    Add project files.

---
+++ NakladakDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using NakladakDemo;
+
+Truck kamion = new Truck(10000);
+Random rand = new Random();
+while (true)
+{
+    Console.Clear();
+    ZobrazKamion(kamion);
+    System.Threading.Thread.Sleep(2000);
+    if(rand.NextDouble() < 0.5)
+    {
+        int naklad = rand.Next(1000, 4000);
+        Console.WriteLine($"Vykládám: {naklad}kg");
+        if (kamion.UnLoad(naklad))
+        {
+            Console.WriteLine("Vyloženo");
+        }
+        else
+        {
+            Console.WriteLine("Nevyloženo");
+        }        
+    }
+    else
+    {
+        int naklad = rand.Next(1000, 4000);
+        Console.WriteLine($"Nakládám: {naklad}kg");
+        int nalozeno = kamion.Load(naklad);
+        Console.WriteLine($"Naloženo {nalozeno}kg");
+    }
+    System.Threading.Thread.Sleep(2000);
+}
+
+
+static void ZobrazKamion(Truck truck)
+{
+    Console.WriteLine($"Kamion - Max nosnost: {truck.MaxLoad}kg");
+    Console.WriteLine($"Kamion - aktuální náklad: {truck.ActualLoad}kg");
+}
---
+++ NakladakDemo/Truck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NakladakDemo
+{
+    public class Truck
+    {
+        public int MaxLoad { get; }
+        public int ActualLoad { get; private set; }
+
+        public Truck(int maxLoad)
+        {
+            MaxLoad = maxLoad;
+        }
+
+        public int Load(int loadWeight)
+        {
+            int loaded = Math.Min(loadWeight, MaxLoad - ActualLoad);
+            ActualLoad += loaded; 
+            return loaded;            
+        }
+
+        public bool UnLoad(int loadWeight)
+        {
+            if(ActualLoad >= loadWeight)
+            {
+                ActualLoad -= loadWeight;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
